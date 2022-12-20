4037f2b0de97b1928f7e224832ffc742a9753318
mahmoud-hus <mahmoud.hussein@dafa.sa>
2022-12-20 13:29:53 +0200

    cookies card

--- components/layouts/CookiesCard.vue
+++ components/layouts/CookiesCard.vue
@@ -1,13 +1,98 @@
 <template>
-  
+  <div class="cookies-card" :class="isShow ? 'active' : 'no-active'">
+    <b-container>
+      <div class="cookies-card-item">
+        <h3 class="cookie-title text-capitalize">{{ $t('allow_cookie') }}</h3>
+        <p class="cookie-desc">
+          {{ $t('we_use_cookie') }}
+          <nuxt-link to="/cookie-policy">{{
+            $t('link_otix_cookie_policy')
+          }}</nuxt-link>
+        </p>
+        <div class="d-flex align-items-center justify-content-end gap-3">
+          <Btn
+            @onClick="handleCookie(true)"
+            :name="$t('ok')"
+            :style="{
+              // backgroundColor: &quot;transparent&quot;,
+              minWidth: 200,
+              // color: &quot;var(--dark)&quot;,
+              // borderColor: &quot;var(--dark)&quot;,
+            }"
+          />
+          <Btn
+            :name="$t('cancel')"
+            @onClick="handleCookie"
+            :style="{
+              backgroundColor: 'transparent',
+              minWidth: 200,
+              color: 'var(--dark)',
+              border: '1px solid var(--dark)',
+            }"
+          />
+        </div>
+      </div>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import Cookies from 'js-cookie'
 export default {
-
+  data() {
+    return {
+      isShow: true,
+    }
+  },
+  methods: {
+    handleCookie(status) {
+      this.isShow = false
+      if (status) {
+        Cookies.set('cookie', 'accepted-cookie')
+      }
+    },
+  },
 }
 </script>
 
 <style>
+.cookies-card {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--bs-white);
+  box-shadow: 0 0 10px rgba(0 0 0 / 0.2);
+  padding: var(--space-lg) 0;
+  /* color: var(--bs-white); */
+  animation: cookieAnimation 1s ease-in-out;
+  transition: 0.7s;
+}
 
+.cookies-card.no-active {
+  bottom: -100%;
+}
+
+.cookie-title {
+  color: var(--primary);
+}
+
+.cookie-desc {
+  color: var(--gray);
+  line-height: 1.7;
+  margin-bottom: var(--space-md);
+}
+
+.cookie-desc a {
+  color: var(--bs-primary);
+}
+
+@keyframes cookieAnimation {
+  from {
+    bottom: -100%;
+  }
+  to {
+    bottom: 0;
+  }
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,20 +3,34 @@
     <HeaderLayout />
     <Nuxt />
     <FooterLayout />
+    <CookiesCardLayout v-if="isSetCookie" />
   </div>
 </template>
 
 <script>
 import HeaderLayout from '~/components/layouts/header/Header.vue'
 import FooterLayout from '~/components/layouts/footer/Footer.vue'
+import CookiesCardLayout from '~/components/layouts/CookiesCard.vue'
+import Cookies from 'js-cookie'
 export default {
-  components: { HeaderLayout, FooterLayout },
+  components: { HeaderLayout, FooterLayout, CookiesCardLayout },
+  data() {
+    return {
+      isSetCookie: true,
+    }
+  },
   mounted() {
     const root = document.documentElement
     const lang = this.$i18n.localeProperties
     root.setAttribute('lang', lang.code)
     root.setAttribute('dir', lang.dir)
     this.$i18n.setLocale(lang.code)
+  },
+  created() {
+    const getCookie = Cookies.get('cookie')
+    if (getCookie) {
+      this.isSetCookie = false
+    }
   },
 }
 </script>
